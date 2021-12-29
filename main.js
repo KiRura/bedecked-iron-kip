712cd0139ac1da1c7a1054940180a052af9f0e1a
Glitch (pumped-chopper) <none>
2021-12-29 22:23:19 +0000

    🏚🌵 Checkpoint ./main.js:16239852/2450

--- main.js
+++ main.js
@@ -25,47 +25,33 @@ client.on('message', message =>
 	}
 });
 
-const ytdl = require('ytdl-core');
- const { entersState, AudioPlayerStatus, createAudioPlayer, createAudioResource, joinVoiceChannel,  StreamType } = require('@discordjs/voice');
- 
- client.on('messageCreate', async message => {
-   // メッセージが "!yt" からはじまっていてサーバー内だったら実行する
-   if (!message.content.startsWith('!yt') || !message.guild) {
-     return;
-   }
-   // メッセージから動画URLだけを取り出す
-   const url = message.content.split(' ')[1];
-   if (!ytdl.validateURL(url)) return message.reply(`${url}は処理できません。`);
-   // コマンドを実行したメンバーがいるボイスチャンネルを取得
-   const channel = message.member.voiceChannel;
-   // コマンドを実行したメンバーがボイスチャンネルに入ってなければ処理を止める
-   if (!channel) return message.reply('先にボイスチャンネルに参加してください！');
-   // チャンネルに参加
-   const connection = joinVoiceChannel({
-    adapterCreator: channel.guild.voiceAdapterCreator,
-    channelId: channel.id,
-    guildId: channel.guild.id,
-    selfDeaf: true,
-    selfMute: false,
-   });
-   const player = createAudioPlayer();
-   connection.subscribe(player);
-   // 動画の音源を取得
-   const stream = ytdl(ytdl.getURLVideoID(url), {
-     filter: format => format.audioCodec === 'opus' && format.container === 'webm', //webm opus
-     quality: 'highest',
-     highWaterMark: 32 * 1024 * 1024, // https://github.com/fent/node-ytdl-core/issues/902
-   });
-   const resource = createAudioResource(stream, {
-      inputType: StreamType.WebmOpus
-    });
-   // 再生
-   player.play(resource);
-   await entersState(player,AudioPlayerStatus.Playing, 10 * 1000);
-   await entersState(player,AudioPlayerStatus.Idle, 24 * 60 * 60 * 1000);
-   // 再生が終了したら抜ける
-   connection.destroy();
- });
+npm i ytdl-core
+const ytdl = require('ytdl-core')
+
+client.on('message', async message => {
+  // メッセージが "!yt" からはじまっていてサーバー内だったら実行する
+  if (message.content.startsWith('!yt') && message.guild) {
+    // メッセージから動画URLだけを取り出す
+    const url = message.content.split(' ')[1]
+    // まず動画が見つからなければ処理を止める
+    if (!ytdl.validateURL(url)) return message.reply('動画が存在しません！')
+    // コマンドを実行したメンバーがいるボイスチャンネルを取得
+    const channel = message.member.voiceChannel
+    // コマンドを実行したメンバーがボイスチャンネルに入ってなければ処理を止める
+    if (!channel) return message.reply('先にボイスチャンネルに参加してください！')
+    // チャンネルに参加
+    const connection = await channel.join()
+    // 動画の音源を取得
+    const stream = ytdl(ytdl.getURLVideoID(url), { filter: 'audioonly' })
+    // 再生
+    const dispatcher = connection.playStream(stream)
+    
+    // 再生が終了したら抜ける
+    dispatcher.on('end', () => {
+      channel.leave()
+    })
+  }
+})
 
 
 if(process.env.DISCORD_BOT_TOKEN == undefined)
