6bc365b779e42bbf791b7028b7a31cdcfb3eb8db
Glitch (pumped-chopper) <none>
2021-12-30 00:39:07 +0000

    👧🚞 Checkpoint ./main.js:16239852/2079 ./.glitch-assets:16239852/339

--- main.js
+++ main.js
@@ -16,19 +16,40 @@ client.on('ready', message =>
   console.log('bot is ready!');
 });
 
-client.on('message', message =>{
-	if(message.isMemberMentioned(client.user) && message.author != client.user){
-  let arr = ["ヒルチャールのお兄さんが病気になった ヒルチャールのお姉さんが看病して〜 ヒルチャールのお兄さんが薬を飲んでも治らなァい！", "ｳｧｰﾝそうですねぇ。あたり、魔神に呪われてます。それもヤバいくらいにぃ。", "あれれェ〜？キョンシーが怖いのォ？", "私とお茶しな〜い？ ｴﾍﾍｪ"];
-  var random = Math.floor(Math.random() * arr.length);
-  var result = arr[random];
-  message.reply(result);
-function lottery(channelId, arr){
-  let random = Math.floor( Math.random() * arr.length);
-  send.message(channelId, arr[random]);
-}
+client.on("message", message => {
+  // bot自身の発言は無視
+  if (message.author.bot) return;
+
+  //　敗北者という単語が含まれていたときの処理
+  if (message.content.includes("敗北者")) {
+    if (message.isMemberMentioned(client.user) && message.member.voiceChannel) {
+      /* ボイスチャンネルにいる人がbotにメンションをしたときの処理 */
+      message.member.voiceChannel
+        .join()
+        .then(connection => {
+          const fileNames = ["nc186194.mp3"];
+          // 複数の音声データのうちいずれかをランダムで選ぶ
+          const fileName =
+            fileNames[Math.floor(Math.random() * fileNames.length)];
+
+          // 再生 再生終了時にボイスチャンネルから切断
+          const dispatcher = connection.playFile(fileName);
+          dispatcher.on("end", () => connection.disconnect());
+        })
+        .catch(console.error);
+    } else {
+      /* 敗北者という単語が含まれているが、
+      メンションでなかったり発言者がボイスチャンネルにいないときの処理 */
+      const texts = [
+        "ハァ...ハァ...敗北者...? 取り消せよ...!!","今の言葉...!!",
+      ];
+
+      // メンションする文字列をランダムで選択
+      const replyText = texts[Math.floor(Math.random() * texts.length)];
+      message.reply(replyText).catch(console.error);
+    }
   }
-}
-          
+});
 
 if(process.env.DISCORD_BOT_TOKEN == undefined)
 {
