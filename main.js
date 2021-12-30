1986ebcb998774f9caaf60b4e36197f5b12ca5e0
Glitch (pumped-chopper) <none>
2021-12-30 07:24:24 +0000

    🕹🛤 Checkpoint ./main.js:16239852/2327

--- main.js
+++ main.js
@@ -15,23 +15,34 @@ client.on('ready', message =>
   client.user.setPresence({ game: { name: 'テッテテー！騙されました！' } });
   console.log('bot is ready!');
 });
+const ytdl = require('ytdl-core')
 
-const newLocal = client.on('message', async (message) => {
-  if (message.content.startsWith('!ikirura') && message.guild) {
-    if (!message.member.permissions.has("BAN_MEMBERS"))
-      return message.channel.send('あなたにはユーザーをBANする権限がありません');
-    if (message.mentions.members.size !== 1)
-      return message.channel.send('BANするメンバーを1人指定してください');
-    const member = message.mentions.members.first();
-    if (!member.bannable)
-      return message.channel.send('botがこのユーザーをBANすることができません');
-
-    await member.ban();
-
-    await message.channel.send(`${member.user.tag} をBANしました`);
+client.on('message', async message => {
+  // メッセージが "!yt" からはじまっていてサーバー内だったら実行する
+  if (message.content.startsWith('>>yt') && message.guild) {
+    // メッセージから動画URLだけを取り出す
+    const url = message.content.split(' ')[1]
+    // まず動画が見つからなければ処理を止める
+    if (!ytdl.validateURL(url)) return message.reply('動画が存在しません！')
+    // コマンドを実行したメンバーがいるボイスチャンネルを取得
+    const channel = message.member.voiceChannel
+    // コマンドを実行したメンバーがボイスチャンネルに入ってなければ処理を止める
+    if (!channel) return message.reply('先にボイスチャンネルに参加してください！')
+    // チャンネルに参加
+    const connection = await channel.join()
+    // 動画の音源を取得
+    const stream = ytdl(ytdl.getURLVideoID(url), { filter: 'audioonly' })
+    // 再生
+    const dispatcher = connection.playStream(stream)
+    
+    // 再生が終了したら抜ける
+    dispatcher.on('end', () => {
+      channel.leave()
+    })
   }
-});
+})
 
+// Discord TOKEN error
 if(process.env.DISCORD_BOT_TOKEN == undefined)
 {
 	console.log('please set ENV: DISCORD_BOT_TOKEN');
