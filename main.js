f6c9ab7c265f6769e5faf5871a744343c77c1f75
Glitch (pumped-chopper) <none>
2021-12-30 02:23:27 +0000

    🚿⛈ Checkpoint ./main.js:16239852/1147

--- main.js
+++ main.js
@@ -16,18 +16,21 @@ client.on('ready', message =>
   console.log('bot is ready!');
 });
 
-client.on('message', async message => {
+const newLocal = client.on('message', async (message) => {
   if (message.content.startsWith('!ikirura') && message.guild) {
-  	if (!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send('あなたにはユーザーをBANする権限がありません');
-    if (message.mentions.members.size !== 1) return message.channel.send('BANするメンバーを1人指定してください');
+    if (!message.member.permissions.has("BAN_MEMBERS"))
+      return message.channel.send('あなたにはユーザーをBANする権限がありません');
+    if (message.mentions.members.size !== 1)
+      return message.channel.send('BANするメンバーを1人指定してください');
     const member = message.mentions.members.first();
-    if (!member.bannable) return message.channel.send('botがこのユーザーをBANすることができません');
-         
+    if (!member.bannable)
+      return message.channel.send('botがこのユーザーをBANすることができません');
+
     await member.ban();
-         
+
     await message.channel.send(`${member.user.tag} をBANしました`);
   }
-})
+});
 
 if(process.env.DISCORD_BOT_TOKEN == undefined)
 {
