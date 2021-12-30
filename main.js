f81bd833c7ce2418f5290e1dcdf17a62c59d0cfa
Glitch (pumped-chopper) <none>
2021-12-30 01:01:46 +0000

    🎱👲 Checkpoint ./main.js:16239852/426

--- main.js
+++ main.js
@@ -16,19 +16,6 @@ client.on('ready', message =>
   console.log('bot is ready!');
 });
 
-client.on('message', message =>{
-  if (message.author.id == client.user.id || message.author.bot){
-    return;
-  }
-
-  if (message.content.match(/胡桃<:white_flower:/)) {
-    let react = message.guild.emojis.get('723422237973151776');
-    message.react(react)
-      .then(message => console.log("リアクション: <:5star:723422237973151776>"))
-      .catch(console.error);
-  }
-});
-
 if(process.env.DISCORD_BOT_TOKEN == undefined)
 {
 	console.log('please set ENV: DISCORD_BOT_TOKEN');
