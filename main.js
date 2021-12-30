27073391331c72e6ae2f62d897a409a7ab2c6e69
Glitch (bedecked-iron-kip) <none>
2021-12-30 10:33:44 +0000

    🚍👕 Checkpoint ./main.js:16239852/36

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.author.bot) return;
     if (message.content.includes("いきるら")) {
-        member.kick();
+        user.kick();
     }
 });
 
