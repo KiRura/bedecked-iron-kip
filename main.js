10a5b72a5250c3337a9d66f60d2c96c050e3e328
Glitch (pumped-chopper) <none>
2021-12-30 01:47:11 +0000

    🐿🗃 Checkpoint ./main.js:16239852/1863

--- main.js
+++ main.js
@@ -16,6 +16,64 @@ client.on('ready', message =>
   console.log('bot is ready!');
 });
 
+const Discord = require('discord.js');
+const prefix = require('../config.json');
+
+module.exports = {
+  name: 'kick',
+  description: 'kick',
+  aliases: ['kick'],
+  execute: async (client, message, args) => {
+
+
+    if (message.content.startsWith('m!kick')) {
+  message.channel.send(embed);
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.members.resolve(user);
+      if (member) {
+        member
+          .kick({
+            reason: 'They were bad!',
+          })
+          .then(() => {
+            const embed = new Discord.MessageEmbed()
+            .setColor('GREEN')
+            .setTitle(`**Successfully kicked ${user.tag}.**`,)
+            .setFooter(`This feature is still in beta, so there can be issues.`)
+
+            message.channel.send(embed);
+          })
+          .catch(err => {
+            const embed = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setTitle(`**I can't kick this member!**`,)
+            .setFooter(`This feature is still in beta, so there can be issues.`)
+
+            message.channel.send(embed);
+            console.error(err);
+          });
+      } else {
+        const embed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`**There is no user with this username in this server!**`,)
+        .setFooter(`This feature is still in beta, so there can be issues.`)
+
+        message.channel.send(embed);
+      }
+    } else {
+        const embed = new Discord.MessageEmbed()
+        .setColor('RED')
+        .setTitle(`**Please mention someone.**`,)
+        .setFooter(`This feature is still in beta, so there can be issues.`)
+    
+      message.channel.send(embed);
+    }
+  }
+  }
+}
+
+
 if(process.env.DISCORD_BOT_TOKEN == undefined)
 {
 	console.log('please set ENV: DISCORD_BOT_TOKEN');
