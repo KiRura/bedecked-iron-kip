cc0320368c5161e2b74963e902cdb2f33b00a7f2
Glitch (bedecked-iron-kip) <none>
2022-01-08 07:38:38 +0000

    🏈🚦 Checkpoint ./main.js:16239852/3685

--- main.js
+++ main.js
@@ -18,140 +18,19 @@ client.on('ready', message =>
 });
 
 //キーワードに反応してメッセージを返す
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("おは")) {
-        message.channel.send('おはよう！歯磨いた？顔洗った？朝ごはん食べた？今日も往生堂は貴方をウェルカムだよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("お早う")) {
-        message.channel.send('おはよう！歯磨いた？顔洗った？朝ごはん食べた？今日も往生堂は貴方をウェルカムだよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("お早よう")) {
-        message.channel.send('おはよう！歯磨いた？顔洗った？朝ごはん食べた？今日も往生堂は貴方をウェルカムだよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("オハヨウ")) {
-        message.channel.send('おはよう！歯磨いた？顔洗った？朝ごはん食べた？今日も往生堂は貴方をウェルカムだよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ｵﾊﾖｳ")) {
-        message.channel.send('おはよう！歯磨いた？顔洗った？朝ごはん食べた？今日も往生堂は貴方をウェルカムだよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("おやす")) {
-        message.channel.send('永遠の眠りに就いてね…！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("お休み")) {
-        message.channel.send('永遠の眠りに就いてね…！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("オヤスミ")) {
-        message.channel.send('永遠の眠りに就いてね…！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("おヤスミ")) {
-        message.channel.send('永遠の眠りに就いてね…！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ｵﾔｽﾐ")) {
-        message.channel.send('永遠の眠りに就いてね…！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("疲れた")) {
-        message.channel.send('往生堂で休んでく？今ならグッズが無料で貰えるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("つかれた")) {
-        message.channel.send('往生堂で休んでく？今ならグッズが無料で貰えるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ツカレタ")) {
-        message.channel.send('往生堂で休んでく？今ならグッズが無料で貰えるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ﾂｶﾚﾀ")) {
-        message.channel.send('往生堂で休んでく？今ならグッズが無料で貰えるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ただいま")) {
-        message.channel.send('おかえり！往生堂で御馳走を用意してるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ただま")) {
-        message.channel.send('おかえり！往生堂で御馳走を用意してるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("タダイマ")) {
-        message.channel.send('おかえり！往生堂で御馳走を用意してるよ！');
-    }
-});
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("ﾀﾀﾞｲﾏ")) {
-        message.channel.send('おかえり！往生堂で御馳走を用意してるよ！');
-    }
-});
+
 client.on('message', message => {
     if (message.author.bot) return;
     if (message.content.includes("やす君")) {
         message.channel.send('やす君？あの人はちょっと関わりにくいよ…てか、ブロックされてて見えない！');
     }
-});
-client.on('message', message => {
     if (message.author.bot) return;
     if (message.content.includes("ゆす")) {
         message.channel.send('やす君？あの人はちょっと関わりにくいよ…てか、ブロックされてて見えない！');
     }
 });
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("殺す")) {
-        message.channel.send('そんな命を軽々と扱わないでよ！？');
-    }
-});
 
 //メッセージに反応して行動を起こす
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (message.content.includes("いきるら")) {
-        user.kick();
-    }
-});
 
 // Discord TOKEN error
 if(process.env.DISCORD_BOT_TOKEN == undefined)
